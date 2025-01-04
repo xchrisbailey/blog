a614435d9331e162e31381c9e9f37cae7264c6a4
Chris Bailey <chris@ghostly.io>
2025-01-04 08:45:23 -0500

    ✨feat: add Cloudflare adapter for deployment
    Add @astrojs/cloudflare adapter and its dependencies to enable deployment
    to Cloudflare Pages. Configure adapter in astro.config.mjs.

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,10 @@ import mdx from '@astrojs/mdx';
 
 import sitemap from '@astrojs/sitemap';
 
+import cloudflare from '@astrojs/cloudflare';
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [mdx(), sitemap()]
+  integrations: [mdx(), sitemap()],
+  adapter: cloudflare()
 });
